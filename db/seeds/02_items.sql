e6ef8b0443765b85f95347ef3ab0728bd5fc7ab0
mclmnop <m_charlebois@hotmail.com>
2021-03-06 10:51:06 -0500

    seed items

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,2 +1,4 @@
 INSERT INTO items (name, description, price, image_url, vendor_id, creation_date, is_active, is_sold) VALUES ('couch', 'red and old', 17000, 'https://bit.ly/3kQZZgN', 1, '2021-03-06T07:00:00.000Z', true, true);
+INSERT INTO items (name, description, price, image_url, vendor_id, creation_date, is_active, is_sold) VALUES ('car', 'jaguar', 600000, 'https://bit.ly/3c6wJyO', 1, '2021-03-08T07:00:00.000Z', true, true);
+INSERT INTO items (name, description, price, image_url, vendor_id, creation_date, is_active, is_sold) VALUES ('house', 'bungalow', 200000, 'https://bit.ly/38fcJJb', 3, '2021-02-10T07:00:00.000Z', true, true);
 
