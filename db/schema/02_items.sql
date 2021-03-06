1c82154d798731b21db3516bc524f9c60323d579
mclmnop <m_charlebois@hotmail.com>
2021-03-06 10:41:29 -0500

    items_db

---
+++ db/schema/02_items.sql
@@ -0,0 +1,15 @@
+-- Drop and recreate Users table (Example)
+
+DROP TABLE IF EXISTS items CASCADE;
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL,
+  image_url VARCHAR(255) NOT NULL,
+  vendor_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  creation_date DATE,
+  is_active VARCHAR(255) NOT NULL DEFAULT TRUE,
+  is_sold VARCHAR(255) NOT NULL DEFAULT TRUE
+);
---
+++ db/seeds/02_items.sql
@@ -0,0 +1,2 @@
+INSERT INTO items (name, description, price, image_url, vendor_id, creation_date, is_active, is_sold) VALUES ('couch', 'red and old', 17000, 'https://bit.ly/3kQZZgN', 1, '2021-03-06T07:00:00.000Z', true, true);
+
