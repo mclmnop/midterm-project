3a83518e58756d05e85e00acf7247a6af2e3ade9
mclmnop <m_charlebois@hotmail.com>
2021-03-06 17:39:42 -0500

    search with get name works

--- routes/items.js
+++ routes/items.js
@@ -15,6 +15,8 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+  // outputs list of all items
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM items;`)
       .then(data => {
@@ -29,9 +31,57 @@ module.exports = (db) => {
       });
   });
 
+  //outputs searched item
   router.get("/search", (req, res) => {
-    const searchWord = req.params.keyword;
-    db.query(`SELECT * FROM items WHERE name LIKE '%car%'`)
+    const searchWord = "%" + req.query.name + "%";
+    let queryParams = [];
+    queryParams.push(searchWord)
+    queryString = `SELECT * FROM items WHERE name LIKE $1`
+    db.query(queryString, queryParams)
+      .then(data => {
+        const items = data.rows;
+        console.log(items);
+        res.json({ items });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/new", (req, res) => {
+    const newItem = [req.params.name, req.params.description];
+    db.query(`INSERT INTO items (name, description, price, image_url, vendor_id, creation_date, is_active, is_sold) VALUES ($1, $2, $3, $4, $5 )`, newItem)
+      .then(data => {
+        const items = data.rows;
+        console.log(items);
+        res.json({ items });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    db.query(`SELECT * FROM items WHERE id = $1`, [req.params.id])
+      .then(data => {
+        const items = data.rows;
+        console.log(items);
+        res.json({ items });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  //receives the things to update on an item
+  router.post("/:id/edit", (req, res) => {
+    db.query(`UPDATE items SET description description = 'new' WHERE id = $1`, [req.params.id])
       .then(data => {
         const items = data.rows;
         console.log(items);
