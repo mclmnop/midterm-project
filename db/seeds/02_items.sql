5b170f4d10f1f0f5b7a185ea37e16eec64e8b30b
Ghulam Murtaza <ghulam.murtaza@mail.mcgill.ca>
2021-03-09 23:12:36 +0000

    views file updated

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -2,3 +2,5 @@ INSERT INTO items (name, description, price, image_url, vendor_id, creation_date
 INSERT INTO items (name, description, price, image_url, vendor_id, creation_date, is_active, is_sold) VALUES ('car', 'jaguar', 600000, 'https://bit.ly/3c6wJyO', 1, '2021-03-08T07:00:00.000Z', true, true);
 INSERT INTO items (name, description, price, image_url, vendor_id, creation_date, is_active, is_sold) VALUES ('house', 'bungalow', 200000, 'https://bit.ly/38fcJJb', 3, '2021-02-10T07:00:00.000Z', true, true);
 
+
+
