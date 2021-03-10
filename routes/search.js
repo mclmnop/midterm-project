965bf67a9f62ab96b04b77351b5d9c5c7f2070b7
mclmnop <m_charlebois@hotmail.com>
2021-03-09 19:46:54 -0500

    added price filter to search query

--- routes/search.js
+++ routes/search.js
@@ -19,16 +19,30 @@ module.exports = (db) => {
 
   //outputs searched item for now works with a name
   router.get("/", (req, res) => {
+    console.log(req.query)
     const searchWord = "%" + req.query.search + "%";
     let queryParams = [];
     queryParams.push(searchWord);
+    if (req.query.search_min_price) {
+      queryParams.push(req.query.search_min_price * 100)
+    } else {
+      queryParams.push(0)
+    }
+    if (req.query.search_max_price) {
+      queryParams.push(req.query.search_max_price * 100)
+    } else {
+      queryParams.push(2147483647)
+    }
     const queryString =
     `
       SELECT *
       FROM items
-      WHERE name LIKE $1
-      OR description LIKE $1
+      WHERE (name LIKE $1
+      OR description LIKE $1)
+      AND price BETWEEN $2 AND $3
+      AND is_active = 'true'
     `;
+    console.log(queryString, queryParams)
     db.query(queryString, queryParams)
       .then(data => {
         const items = data.rows;
